4482283e4947149010f790fd508de7348920bd0c
Shaun-Sim-SIMTech <shaun_sim@simtech.a-star.edu.sg>
2022-05-10 18:17:21 +0800

    done

---
+++ question 3a/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQConsumer;
+
+
+
+IHost host = Host.CreateDefaultBuilder(args)
+.ConfigureServices(services =>
+{
+    services.AddHostedService<ConsumerService>();
+})
+.Build();
+
+
+
+await host.RunAsync();
